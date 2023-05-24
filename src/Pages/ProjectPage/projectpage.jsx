0df4cbe6a1808f2b2680828ba1c02bceebbb22a5
Kristýna Nováková <novakovkri@gmail.com>
2023-05-24 14:44:04 +0200

    Adding Prototype Photo

--- src/Pages/ProjectPage/projectpage.jsx
+++ src/Pages/ProjectPage/projectpage.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import './style.css';
+import prototype from './img/prototype.png';
 
 export const ProjectPage = () => {
   return (
     <div className="page">
       <div className="logo" />
-      <h1 className="title">Slow Wear</h1>
+
       <div className="content">
-        <h2 className="section-title">Popis projektu</h2>
-        <p>
-          E-shop Slow Wear funguje jako půjčovna oblečení na principu měsíčního
-          poplatku. Zákazníci si mohou každý měsíc vybrat 6 různých kousků
-          oblečení, které po 30 dnech vrátí a objednají si další. Cílem projektu
-          je pomoci zredukovat množství oblečení v našich skříních, které
-          neunosíme, a tím podpořit udržitelnost a zodpovědný přístup k módnímu
-          průmyslu.
-        </p>
-        <p>
-          Další informace o projektu a jeho funkcích můžete nalézt na GitHubu:
-        </p>
-        <a href="odkaz_na_github" className="link">
-          GitHub
-        </a>
+        <div className="text">
+          <h1 className="title">Slow Wear</h1>
+          <h2 className="section-title">Popis projektu</h2>
+          <p>
+            E-shop Slow Wear funguje jako půjčovna oblečení na principu
+            měsíčního poplatku. Zákazníci si mohou každý měsíc vybrat 6 různých
+            kousků oblečení, které po 30 dnech vrátí a objednají si další. Cílem
+            projektu je pomoci zredukovat množství oblečení v našich skříních,
+            které neunosíme, a tím podpořit udržitelnost a zodpovědný přístup k
+            módnímu průmyslu.
+          </p>
+          <h2 className="section-title">Autorky projektu</h2>
+          <ul>
+            <li>Kristýna Nováková</li>
+            <li>Linda Balounová</li>
+          </ul>
+          <h2 className="section-title">Další informace</h2>
+          <p>
+            Další informace o projektu a jeho funkcích můžete nalézt na GitHubu:
+          </p>
+          <a href="odkaz_na_github" className="link">
+            GitHub
+          </a>
+        </div>
+        <div className="prototypeImage">
+          <img src={prototype} alt="prototyp" />
+        </div>
       </div>
     </div>
   );
