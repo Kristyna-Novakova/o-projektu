baf27b49a59a35297ecfe569a87393802201a153
Kristýna Nováková <novakovkri@gmail.com>
2023-05-26 11:59:00 +0200

    installing react-icons

--- src/Pages/AuthorPage/index.jsx
+++ src/Pages/AuthorPage/index.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { authors } from '../author-list';
 import './style.css';
+import { AiFillGithub } from 'react-icons/ai';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { MdEmail } from 'react-icons/md';
 
 export const AuthorPage = () => {
   const { authorId } = useParams();
@@ -14,16 +17,33 @@ export const AuthorPage = () => {
   return (
     <div className="author-page">
       <h1>{authorDetail.name}</h1>
-      <div className="textAboutAuthor">
+      <div className="author-info">
         {' '}
-        <h2>Informace o autorce</h2>
-        <p>{authorDetail.about}</p>
-        <h2>Odkaz na další projekty:</h2>
-        <a href={authorDetail.link} target="_blank">
-          GitHub
-        </a>
+        <div className="about">
+          <h2>Informace o autorce</h2>
+          <p>{authorDetail.about}</p>
+          <h2>Odkaz na další projekty:</h2>
+          <ul>
+            <li>
+              <AiFillGithub />{' '}
+              <a href={authorDetail.github} target="_blank">
+                GitHub
+              </a>
+            </li>
+            <li>
+              <AiFillLinkedin />
+              <a href={authorDetail.linkedin} target="_blank">
+                LinkedIn profil
+              </a>
+            </li>
+            <li>
+              <MdEmail />
+              <a href="mailto: {authorDetail.email}">{authorDetail.email}</a>
+            </li>
+          </ul>
+        </div>
+        <img src={authorDetail.picture} alt={authorDetail.name} />
       </div>
-      <img src={authorDetail.picture} alt={authorDetail.name} />
     </div>
   );
 };
