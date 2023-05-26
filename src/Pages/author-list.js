a53d58ba58ca90070c2c706e4b1df02816fde62b
Kristýna Nováková <novakovkri@gmail.com>
2023-05-26 12:13:53 +0200

    adding icons to AuthorPage

--- src/Pages/author-list.js
+++ src/Pages/author-list.js
@@ -9,7 +9,7 @@ export const authors = [
     about:
       'Jsem Týna a od narození mám v sobě cestovatelského červa. K programování jsem si našla cestu skrze svou předchozí práci v CoolPeople, kde jsem pracovala na pozici Content Marketing Specialist. Tak dlouho jsem psala pracovní nabídky pro ajťáky, až jsem se chtěla stát jednou z nich.',
     github: 'https://github.com/Kristyna-Novakova',
-    linkedin: 'linkedin.com/in/kristynanov/',
+    linkedin: 'https://www.linkedin.com/in/kristynanov/',
     email: 'novakovakri@gmail.com',
   },
   {
@@ -19,7 +19,7 @@ export const authors = [
     about:
       'Jsem Linda, je mi 31, bydlím v Praze pátým rokem, předtím jsem žila rok ve Španělsku a dva roky v US, kde jsem pracovala jako au-pair. Pracuju jako office manager v online marketingový agentuře. Dělala jsem na technické podpoře a na HR oddělení v jednom start upu, ale taky jsem jednu sezónu létala a plnila si sen.',
     github: 'https://github.com/lindabalounova1',
-    linkedin: 'linkedin.com/in/linda-balounova/',
+    linkedin: 'https://www.linkedin.com/in/linda-balounova/',
     email: 'linda-balounova@instinct.ag',
   },
 ];
